b0345279840949f900f2c0bff2bb51c3ab96efb9
Sabina Shrestha <sabina.pari@yahoo.com>
2018-05-11 00:55:01 -0600

    clean file

--- calculator.rb
+++ calculator.rb
@@ -1,95 +1,6 @@
-#take the first number
-#take the last numbe
-#puts result
-#take modifier
-
-
-# def choose_one
-#   puts "Type first number to calculate."
-#   @first = gets.to_i
-#
-#   puts "Choose your modifier. +  -  * / %"
-#   @modifier = gets.chomp
-#
-#   evaluate_user
-#
-#   puts "Type last number to calculate."
-#   @second = gets.to_i
-# end
-
-# def mod
-#   # get modifier
-#   # fails call this method again
-# end
-
-# def calc_again
-#   puts "would you like to calculate again? Type Y or N"
-#   @user = gets.chomp.downcase
-#
-#   if @user == "y"
-#     run_calculator
-#   else
-#     puts "Good Bye"
-#     exit
-#   end
-# end
-
-# case statement
-# if @modifier == "+"
-#     @result = @result + @second
-# elsif @modifier == "-"
-#     @result = @result - @second
-# elsif @modifier == "*"
-#     @result = @result * @second
-# elsif @modifier == "/"
-#     @result = @result / @second
-# else
-#     @result = @result % @second
-# end
-
-  # @number_1 = @result
-
-  #case @modifier
-  #   when "+"
-  #     @result = @result + @second
-  #   when "-"
-  #     @result = @result - @second
-  #   when "*"
-  #     @result = @result * @second
-  #   when "/"
-  #     @result = @result / @second
-  #   else
-  #     @result = @result % @second
-  # end
-
-  # if @modifier == "+"
-  #     @result = @first + @second
-  # elsif @modifier == "-"
-  #     @result = @first - @second
-  # elsif @modifier == "*"
-  #     @result = @first * @second
-  # elsif @modifier == "/"
-  #     @result = @first / @second
-  # else
-  #     @result = @first % @second
-  # end
-
-  # case @modifier
-  #   when "+"
-  #     @result = @first + @second
-  #   when "-"
-  #     @result = @first - @second
-  #   when "*"
-  #     @result = @first * @second
-  #   when "/"
-  #     @result = @first / @second
-  #   else
-  #     @result = @first % @second
-  # end
 
 require 'pry'
 
-#@modify = ["+", "-", "*", "/", "%"]
 
 def first_number
   puts "Type first number to calculate."
@@ -114,18 +25,11 @@ end
 
 
 def evaluate_user
-  #binding.pry
-  # [] ruby method to see if contains
-  # unless @modifier == "+" || @modifier == "-" || @modifier == "*" || @modifier == "/" || @modifier == "%"
-    # @modify.include?(@modifier)
     is_modifier = ["+", "-", "*", "/", "%"].include?(@modifier)
     if(!is_modifier)
       puts "invalid entry. Try again."
       get_modifier
     end
-
-    #run_calculator
-
 end
 
 def number1
